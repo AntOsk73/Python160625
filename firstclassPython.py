118a3ba099adcb993d8f7c26120899bb89827b3e
Antoine Osky <antoine.osky@live.com>
2025-06-16 20:10:12 -0400

    mycomits

--- firstclassPython.py
+++ firstclassPython.py
@@ -20,3 +20,9 @@ if num % 2 == 0:
     print("Even")
 else:
     print("Odd")
+
+    num = int(input("Enter a number: "))
+if num % 5 == 0:
+    print("you're free to")
+else:
+    print("sorry, you stay in prison")
