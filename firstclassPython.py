3d497b9d3cb8b867bb114e0d9f850bb3c1187a1b
Antoine Osky <antoine.osky@live.com>
2025-06-18 18:15:46 -0400

    mycommits

--- firstclassPython.py
+++ firstclassPython.py
@@ -29,3 +29,47 @@ else:
 
 import random
 print("Random number:", random.randint(1, 100))
+
+
+print(" Welcome to the Guessing Game!")
+secret_number = random.randint(1, 10)
+attempts = 3
+ 
+while attempts > 0:
+    guess = int(input("Guess a number between 1 and 10: "))
+    if guess == secret_number:
+        print("🎉 Correct! You win!")
+        break
+    else:
+        attempts -= 1
+        if guess < secret_number:
+            print("Too low!")
+        else:
+            print("Too high!")
+ 
+        if attempts > 0:
+            print(f"Try again! {attempts} attempts left.")
+        else:
+            print(f" Out of tries! The number was {secret_number}.")
+
+    # Asking for user input
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+# Displaying the result
+print(f"Hello, {name}! You are {age} years old.")
+
+# Asking for user input
+name = input("What is your name? ")
+print(f"Thank you, {name}!")
+
+job = input("What is your job? ")
+salary = input("What is your salary? ")
+location = input("Where are you located? ")
+
+# Displaying the final message
+print(f"I, {name}, from {location}, working as a {job}, with the salary of {salary}, would love to work in cloud and AI.")
+
+
+
+        
