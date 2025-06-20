8b754e40f493ec560d150f507d01f8f1dac4f255
Antoine Osky <antoine.osky@live.com>
2025-06-20 18:43:35 -0400

    mycommits

---
+++ june20.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    return int(a + b)
+number1=int(input("Give me the first number:"))
+number2=int(input("Give me the second number:"))
+addition=add_numbers(number1,number2)
+print("The sum is",addition)
+
+
+
